b6701d166040279d723ecb8d13940bd3b4426684
Leon Starr <leon_starr@modelint.com>
2025-05-08 22:36:59 -0700

    Rename action works

--- src/mx/actions/rename.py
+++ src/mx/actions/rename.py
@@ -41,7 +41,21 @@ class Rename(Action):
         if self.activity.xe.debug:
             Relation.print(db=mmdb, variable_name=rename_table_action_rv)
 
-        # Join with Table Action
+        # Extract input and output flows required by the Traversal Action
+        rename_values = rename_table_action_t.body[0]  # convenient abbreviation of the rename table action tuple body
+        self.source_flow_name = rename_values["Input_a_flow"]  # Name like F1, F2, etc
+        self.source_flow = self.activity.flows[self.source_flow_name]  # The active content of source flow (value, type)
+        # Just the name of the destination flow since it isn't enabled until after the Traversal Action executes
+        self.dest_flow_name = rename_values["Output_flow"]
+        # And the output of the Rename will be placed in the Activity flow dictionary
+        # upon completion of this Action
 
-        pass
+        # Rename
+        rename_output_rv = RVN.name(db=mmdb, name="rename_output")
+        Relation.rename(db=self.domdb, names={rename_values["From_attribute"]: rename_values["To_attribute"]},
+                        relation=self.source_flow.flowtype, svar_name=rename_output_rv)
+        if self.activity.xe.debug:
+            Relation.print(db=self.domdb, variable_name=rename_output_rv)
+
+        self.activity.flows[self.dest_flow_name] = ActiveFlow(value=rename_output_rv, flowtype=rename_values["To_table"])
 
