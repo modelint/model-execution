bb08e1bc38cb64c9ca126d9f7e1362db218e8979
Leon Starr <leon_starr@modelint.com>
2025-06-07 20:35:53 -0700

    set union operation logs ok

--- src/mx/actions/set_action.py
+++ src/mx/actions/set_action.py
@@ -13,7 +13,7 @@ from pyral.database import Database  # Diagnostics
 # MX
 from mx.db_names import mmdb
 from mx.actions.action import Action
-from mx.actions.flow import ActiveFlow
+from mx.actions.flow import ActiveFlow, FlowDir
 from mx.rvname import declare_rvs
 
 # See comment in scalar_switch.py
@@ -60,23 +60,32 @@ class SetAction(Action):
                           svar_name=mmrv.this_set_action)
 
         # Join it with the Table Action superclass to get the input / output flows
-        set_table_action_t = Relation.join(db=mmdb, rname1=mmrv.this_set_action, rname2="Table_Action",
-                                              svar_name=mmrv.set_table_action)
+        set_table_action_r = Relation.join(db=mmdb, rname1=mmrv.this_set_action, rname2="Table_Action",
+                                           svar_name=mmrv.set_table_action)
         if self.activity.xe.debug:
             Relation.print(db=mmdb, variable_name=mmrv.set_table_action)
 
         # Extract input and output flows required by the Union Action
-        set_table_action_values = set_table_action_t.body[0]  # convenient abbreviation of the rename table action tuple body
-        self.source_A_name = set_table_action_values["Input_a_flow"]  # Name like F1, F2, etc
-        self.source_B_name = set_table_action_values["Input_b_flow"]
+        set_table_action_t = set_table_action_r.body[0]
+        self.activity.xe.mxlog.log(message=f"- Operation: {set_table_action_t["Operation"]}")
+        self.activity.xe.mxlog.log(message="Flows")
+        # convenient abbreviation of the rename table action tuple body
+        self.source_A_name = set_table_action_t["Input_a_flow"]  # Name like F1, F2, etc
+        self.source_B_name = set_table_action_t["Input_b_flow"]
         self.source_A_flow = self.activity.flows[self.source_A_name]  # The active content of source flow (value, type)
         self.source_B_flow = self.activity.flows[self.source_B_name]  # The active content of source flow (value, type)
+        self.activity.xe.mxlog.log_nsflow(flow_name=self.source_A_name, flow_dir=FlowDir.IN,
+                                          flow_type=self.source_A_flow.flowtype, activity=self.activity,
+                                          db=self.domdb, rv_name=self.source_A_flow.value)
+        self.activity.xe.mxlog.log_nsflow(flow_name=self.source_B_name, flow_dir=FlowDir.IN,
+                                          flow_type=self.source_B_flow.flowtype, activity=self.activity,
+                                          db=self.domdb, rv_name=self.source_B_flow.value)
         # Just the name of the destination flow since it isn't enabled until after the Traversal Action executes
-        self.dest_flow_name = set_table_action_values["Output_flow"]
+        self.dest_flow_name = set_table_action_t["Output_flow"]
         # And the output of the Set Action will be placed in the Activity flow dictionary
         # upon completion of this Action
 
-        set_action_name = set_table_action_t.body[0]["Operation"]
+        set_action_name = set_table_action_r.body[0]["Operation"]
         set_action_output_drv = Relation.declare_rv(db=self.domdb, owner=self.rvp, name="set_action_output")
         if set_action_name == "UNION":
             Relation.union(db=self.domdb, relations=(self.source_A_flow.value, self.source_B_flow.value),
@@ -95,6 +104,9 @@ class SetAction(Action):
         self.activity.flows[self.dest_flow_name] = ActiveFlow(value=set_action_output_drv,
                                                               flowtype=self.source_A_flow.flowtype)
 
+        self.activity.xe.mxlog.log_nsflow(flow_name=self.dest_flow_name, flow_dir=FlowDir.OUT,
+                                          flow_type=self.source_A_flow.flowtype, activity=self.activity,
+                                          db=self.domdb, rv_name=set_action_output_drv)
         # This action's mmdb rvs are no longer needed)
         Relation.free_rvs(db=mmdb, owner=self.rvp)
 
