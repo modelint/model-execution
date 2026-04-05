0812487f9ee0a0779798f8568d3ce0749f75f0c7
Leon Starr <leon_starr@modelint.com>
2026-04-05 10:27:25 -0700

    Updated select.py to new activity_execution and logging format

--- src/mx/actions/select.py
+++ src/mx/actions/select.py
@@ -1,39 +1,41 @@
 """ select.py -- executes the Select Action """
 
 # System
+import logging
 from typing import TYPE_CHECKING, NamedTuple
 
 if TYPE_CHECKING:
-    from mx.method_execution import MethodExecution  # TODO: Replace with Activity after refactoring State/Assigner Activities
+    from mx.activity_execution import ActivityExecution
 
 # Model Integration
 from pyral.relation import Relation
 from pyral.database import Database  # Diagnostics
 
 # MX
+from mx.log_table_config import TABLE, log_table
 from mx.db_names import mmdb
 from mx.actions.action_execution import ActionExecution
 from mx.actions.flow import ActiveFlow, FlowDir
 from mx.rvname import declare_rvs
 from mx.instance_set import InstanceSet
+from mx.utility import *
 
+_logger = logging.getLogger(__name__)
 
 # See comment in scalar_switch.py
-class RVs(NamedTuple):
-    activity_select_actions: str
-    this_select_action: str
+class MMRVs(NamedTuple):
+    select_action: str
     restriction_condition: str
     my_criteria: str
     my_eq_criteria: str
     my_comp_criteria: str
 
-
 # This wrapper calls the imported declare_rvs function to generate a NamedTuple instance with each of our
 # variables above as a member.
-def declare_my_module_rvs(db: str, owner: str) -> RVs:
-    rvs = declare_rvs(db, owner, "activity_select_actions", "this_select_action",
-                      "restriction_condition", "my_criteria", "my_eq_criteria", "my_comp_criteria")
-    return RVs(*rvs)
+def declare_my_module_rvs(db: str, owner: str) -> MMRVs:
+    rvs = declare_rvs(db, owner, "select_action", "restriction_condition",
+                      "my_criteria", "my_eq_criteria", "my_comp_criteria")
+    return MMRVs(*rvs)
 
 
 def str_to_bool(s: str) -> bool:
@@ -52,7 +54,7 @@ def str_to_bool(s: str) -> bool:
 
 class Select(ActionExecution):
 
-    def __init__(self, action_id: str, activity: "MethodExecution"):
+    def __init__(self, action_id: str, activity: "ActivityExecution"):
         """
         Perform the Select Action on a domain model.
 
@@ -60,7 +62,7 @@ class Select(ActionExecution):
         :param activity: A<n> Activity ID (for Method and State Activities)
         """
 
-        super().__init__(activity=activity, anum=activity.anum, action_id=action_id)
+        super().__init__(activity_execution=activity, action_id=action_id)
 
         # Do not execute this Action if it is not enabled, see comment in Action class
         if self.disabled:
@@ -68,38 +70,34 @@ class Select(ActionExecution):
 
         # Get a NamedTuple with a field for each relation variable name
         self.mmrv = declare_my_module_rvs(db=mmdb, owner=self.rvp)
-        mmrv = self.mmrv  # For brevity
+        mmrv = self.mmrv
 
         # Lookup the Action instance
-        # Start with all Select actions in this Activity
-        Relation.semijoin(db=mmdb, rname1=activity.method_rvname, rname2="Select_Action",
-                          svar_name=mmrv.activity_select_actions)
-        # Narrow it down to this Select Action instance
-        R = f"ID:<{action_id}>"
-        select_action_r = Relation.restrict(db=mmdb, relation=mmrv.activity_select_actions, restriction=R,
-                                            svar_name=mmrv.this_select_action)
+        select_action_r = Relation.semijoin(
+            db=mmdb, rname1=self.action_mmrv, rname2="Select Action", svar_name=mmrv.select_action)
+        log_table(_logger, table_msg(db=mmdb, variable_name=mmrv.select_action))
         select_action_t = select_action_r.body[0]
-        if self.activity.xe.debug:
-            Relation.print(db=mmdb, variable_name=mmrv.this_select_action)
 
         self.source_flow_name = select_action_t["Input_flow"]
-        self.source_flow = self.activity.flows[self.source_flow_name]  # The active content of source flow (value, type)
-        if self.activity.xe.debug and self.source_flow.value:
-            Relation.print(db=self.domdb, variable_name=self.source_flow.value)
-
+        self.source_flow = self.activity_execution.flows[self.source_flow_name]  # The active content of source flow (value, type)
+        _logger.info(f"{self.source_flow_name}")
+        # _logger.info("Flows")
+        # log_table(_logger, nsflow_msg(db=self.domdb, flow_name=self.source_flow_name, flow_dir=FlowDir.IN,
+        #                               flow_type=self.source_flow.flowtype,
+        #                               activity=self.activity_execution, rv_name=self.source_flow.value))
+        #
         # Get the destination flow name
-        subclass_r = Relation.semijoin(db=mmdb, rname1=mmrv.this_select_action, rname2="Single_Select")
+        subclass_r = Relation.semijoin(db=mmdb, rname1=mmrv.select_action, rname2="Single_Select")
         if not subclass_r.body:
-            subclass_r = Relation.semijoin(db=mmdb, rname1=mmrv.this_select_action, rname2="Many_Select")
+            subclass_r = Relation.semijoin(db=mmdb, rname1=mmrv.select_action, rname2="Many_Select")
         self.dest_flow_name = subclass_r.body[0]["Output_flow"]
 
         # Get the Restriction Condition
-        rcond_r = Relation.semijoin(db=mmdb, rname1=mmrv.this_select_action, rname2="Restriction Condition",
+        rcond_r = Relation.semijoin(db=mmdb, rname1=mmrv.select_action, rname2="Restriction Condition",
                                     attrs={"ID": "Action", "Activity": "Activity", "Domain": "Domain"},
                                     svar_name=mmrv.restriction_condition)
-        if self.activity.xe.debug:
-            Relation.print(db=mmdb, variable_name=mmrv.restriction_condition)
-        self.activity.xe.mxlog.log(message=f"- Card: {rcond_r.body[0]["Selection_cardinality"]}")
+        log_table(_logger, table_msg(db=mmdb, variable_name=mmrv.restriction_condition))
+        _logger.info(f"- Card: {rcond_r.body[0]["Selection_cardinality"]}")
 
         # The supplied expression helps us define any complex boolean logic
         # in the restriction phrase to be created.
@@ -107,8 +105,7 @@ class Select(ActionExecution):
 
         # Get all Criteria
         Relation.semijoin(db=mmdb, rname1=mmrv.restriction_condition, rname2="Criterion", svar_name=mmrv.my_criteria)
-        if self.activity.xe.debug:
-            Relation.print(db=mmdb, variable_name=mmrv.my_criteria)
+        log_table(_logger, table_msg(db=mmdb, variable_name=mmrv.my_criteria))
 
         # Make a phrase for each criterion
         # TODO: incorporate and/or/not logic
@@ -117,47 +114,43 @@ class Select(ActionExecution):
         eq_phrases = self.make_eq_phrases()
         comp_phrases = self.make_comparison_phrases()
         criteria_phrases = eq_phrases + comp_phrases
-        self.activity.xe.mxlog.log(message=f"- Criteria: {criteria_phrases}")
-        self.activity.xe.mxlog.log(message="Flows")
+        _logger.info(f"- Criteria: {criteria_phrases}")
+        _logger.info("Flows")
 
-        self.activity.xe.mxlog.log_nsflow(flow_name=self.source_flow_name, flow_dir=FlowDir.IN,
-                                          flow_type=self.source_flow.flowtype, activity=self.activity,
-                                          db=self.domdb, rv_name=self.source_flow.value)
+        log_table(_logger, nsflow_msg(db=self.domdb, flow_name=self.source_flow_name, flow_dir=FlowDir.IN,
+                                      flow_type=self.source_flow.flowtype,
+                                      activity=self.activity_execution, rv_name=self.source_flow.value))
 
         # Convert input irefs to instances and save in same rv
-        input_iset_rv = Relation.declare_rv(db=self.domdb, owner=self.rvp, name="selection_input")
+        input_iset_rv = Relation.declare_rv(db=self.domdb, owner=self.owner, name="selection_input")
         InstanceSet.instances(db=self.domdb, irefs_rv=self.source_flow.value, iset_rv=input_iset_rv,
                               class_name=self.source_flow.flowtype)
 
         # Perform the selection
-        selection_output_rv = Relation.declare_rv(db=self.domdb, owner=self.rvp, name="selection_output")
+        selection_output_drv = Relation.declare_rv(db=self.domdb, owner=self.owner, name="selection_output")
         R = ', '.join(criteria_phrases)  # For now we will just and them all together using commas
         Relation.restrict(db=self.domdb, relation=input_iset_rv, restriction=R.strip(),
-                          svar_name=selection_output_rv)
+                          svar_name=selection_output_drv)
 
-        if self.activity.xe.debug:
-            Relation.print(db=self.domdb, variable_name=selection_output_rv)
+        log_table(_logger, table_msg(db=mmdb, variable_name=selection_output_drv))
 
         # Extract irefs for output
-        InstanceSet.irefs(db=self.domdb, iset_rv=selection_output_rv, irefs_rv=selection_output_rv,
-                          class_name=self.source_flow.flowtype, domain_name=self.activity.domain_name)
+        InstanceSet.irefs(db=self.domdb, iset_rv=selection_output_drv, irefs_rv=selection_output_drv,
+                          class_name=self.source_flow.flowtype,
+                          domain_name=self.activity_execution.domain.name)
 
-        if self.activity.xe.debug:
-            Relation.print(db=self.domdb, variable_name=selection_output_rv)
+        log_table(_logger, table_msg(db=mmdb, variable_name=selection_output_drv))
 
         # Assign result to output flow
         # For a select action, the source and dest flow types must match
-        self.activity.flows[self.dest_flow_name] = ActiveFlow(value=selection_output_rv,
-                                                              flowtype=self.source_flow.flowtype)
+        self.activity_execution.flows[self.dest_flow_name] = ActiveFlow(
+            value=selection_output_drv, flowtype=self.source_flow.flowtype)
 
-        self.activity.xe.mxlog.log_nsflow(flow_name=self.dest_flow_name, flow_dir=FlowDir.OUT,
-                                          flow_type=self.source_flow.flowtype, activity=self.activity,
-                                          db=self.domdb, rv_name=selection_output_rv)
-        # This action's mmdb rvs are no longer needed)
-        Relation.free_rvs(db=mmdb, owner=self.rvp)
-        _rv_after_mmdb_free = Database.get_rv_names(db=mmdb)
-        _rv_after_dom_free = Database.get_rv_names(db=self.domdb)
-        pass
+        log_table(_logger, nsflow_msg(db=self.domdb, flow_name=self.dest_flow_name, flow_dir=FlowDir.OUT,
+                                      flow_type=self.source_flow.flowtype,
+                                      activity=self.activity_execution, rv_name=selection_output_drv))
+
+        self.complete()
 
     def make_eq_phrases(self) -> list[str]:
         """
@@ -196,22 +189,19 @@ class Select(ActionExecution):
         my_comp_criteria_r = Relation.semijoin(db=mmdb, rname1=mmrv.my_criteria, rname2="Comparison_Criterion",
                                                svar_name=mmrv.my_comp_criteria)
 
-        if self.activity.xe.debug:
-            Relation.print(db=mmdb, variable_name=mmrv.my_comp_criteria)
+        log_table(_logger, table_msg(db=mmdb, variable_name=mmrv.my_comp_criteria))
 
         criteria_rphrases: list[str] = []
 
         for c in my_comp_criteria_r.body:
             attr = c['Attribute'].replace(' ', '_')
             scalar_flow_name = c['Value']
-            value = self.activity.flows[scalar_flow_name].value
+            value = self.activity_execution.flows[scalar_flow_name].value
             relop = c['Comparison']
             pyral_op = ':' if relop == '==' and isinstance(value, str) else relop
             # PyRAL specifies boolean values using ptyhon bool type, not strings
             # PyRAL uses ":" for string matches and "==" for numeric matches, so we need to determine the type
             # of the value
-
-
 
             phrase = f"{attr}{pyral_op}<{value}>"
             criteria_rphrases.append(phrase)
